91646e3c8f984512e73c030c1a2ef1d966d397ca
ericksa1 <132090323+ericksa1@users.noreply.github.com>
2024-12-20 19:40:24 +0000

    # Please enter the commit message for your changes. Lines starting

--- Erick's Vite React App/src/main.jsx
+++ Erick's Vite React App/src/main.jsx
@@ -2,9 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Welcome from './pages/Welcome.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+
+  /* can also use the  Welcome form pages */
+
   </StrictMode>,
 )
+
