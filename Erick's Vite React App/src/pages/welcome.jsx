b066fae1cf38699f298da9752211316c8048d80b
ericksa1 <132090323+ericksa1@users.noreply.github.com>
2024-12-20 19:38:59 +0000

    more

---
+++ Erick's Vite React App/src/pages/welcome.jsx
@@ -0,0 +1,20 @@
+function Welcome(){
+
+    return (
+
+        <> 
+        
+        
+             <h1>
+            Welcome Everyone Lets build a new compnent!!!!
+            </h1>  
+            
+            
+            
+            
+        </>
+    )
+
+}
+
+export default Welcome
